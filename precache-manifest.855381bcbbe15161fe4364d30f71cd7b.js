5a6514a4cdfff5c9692d217f11d0a793fd54c53c
Mathieu Enault <me94110@gmail.com>
2020-11-21 18:35:05 +0100

    Updates

---
+++ precache-manifest.855381bcbbe15161fe4364d30f71cd7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3a37ebe9e1765ba8fb5bb97e6462bd",
+    "url": "//Mathieu94110.github.io/my_Pokedex/index.html"
+  },
+  {
+    "revision": "8f5e685d4f66f21bf09e",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fa7ddcf7af8dc6d9ae72",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/css/main.d9624442.chunk.css"
+  },
+  {
+    "revision": "8f5e685d4f66f21bf09e",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/js/2.73e702c7.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/js/2.73e702c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7ddcf7af8dc6d9ae72",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/js/main.6122a139.chunk.js"
+  },
+  {
+    "revision": "d1045c0d0696b4f5cd13",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/js/runtime-main.53c59ac6.js"
+  },
+  {
+    "revision": "a7865c2577e736305ec7e2e11b86922f",
+    "url": "//Mathieu94110.github.io/my_Pokedex/static/media/pokemon_background.a7865c25.jpg"
+  }
+]);
